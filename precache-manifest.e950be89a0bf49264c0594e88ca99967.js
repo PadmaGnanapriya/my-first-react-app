e5a7f8b003df12ead9cacdc45a06df962a164da8
PadmaGnanapriya <padmaisuru@gmail.com>
2020-10-04 21:20:03 +0530

    Updates

---
+++ precache-manifest.e950be89a0bf49264c0594e88ca99967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518deea47a1f20097ea27a2f127a7356",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a312d2ad9d5fcea9a32",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "d680c244f28df47c5a45",
+    "url": "/static/js/2.762adfc5.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.762adfc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a312d2ad9d5fcea9a32",
+    "url": "/static/js/main.e9b626bb.chunk.js"
+  },
+  {
+    "revision": "d04efe293546dd98049a",
+    "url": "/static/js/runtime-main.73f4471a.js"
+  }
+]);
